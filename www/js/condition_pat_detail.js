c63a7f6e0a12b8b47695f68107fb8518dc079859
thirumalacpl <thirumal.acpl@gmail.com>
2016-09-29 11:46:40 +0530

    1

--- www/js/condition_pat_detail.js
+++ www/js/condition_pat_detail.js
@@ -77,14 +77,14 @@ for(a=0;a<patient_detaias_array.length;a++){
 
 //alert('asdkjfgksdafjidkaisf');
 //alert(navigator.geolocation+'navigator.geolocation');
-alert("navigator.geolocation 1");
-alert(navigator.geolocation+'2');
-alert("3");
-alert(navigator.geolocation.getCurrentPosition+'4 watchPosition');
+//alert("navigator.geolocation 1");
+//alert(navigator.geolocation+'2');
+//alert("3");
+//alert(navigator.geolocation.getCurrentPosition+'4 watchPosition');
    navigator.geolocation.getCurrentPosition(onSuccess, onError);  
-alert('as');
+//alert('as');
       function onSuccess(position) {
-alert("onSuccess insuide");
+//alert("onSuccess insuide");
         lat_hosp = position.coords.latitude;
         long_hosp = position.coords.longitude;
 
@@ -111,11 +111,11 @@ var long_hosp ='234';*/
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-     alert('geo err');
+     //alert('geo err');
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
-alert('on gps');
+//alert('on gps');
        function onSuccesspatid(data){
 //alert('Submitted Successfully');
 $("#myPopup1d").popup("open");
